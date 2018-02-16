5c1499681c7b76b2a3e0d6046cd9946fcb993435
Jordan Phoenix <jordan.phoenix1993@gmail.com>
2018-02-16 22:14:36 +0000

    Added Fade Transition Logic.

---
+++ GamesPlusJam/Assets/Scripts/UI/FadeTransition.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeTransition : MonoBehaviour {
+
+	public bool FadeOn = false;
+
+	private float fadeTimeCurrent;
+
+	public float fadeTimeMax = 2.0f;
+
+	private Image fadeImage;
+
+	void Start()
+	{
+		fadeImage = GetComponentInChildren<Image> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (FadeOn)
+		{
+			if (fadeTimeCurrent < fadeTimeMax)
+			{
+				fadeTimeCurrent = Mathf.Clamp(fadeTimeCurrent + Time.deltaTime, 0.0f, fadeTimeMax);
+				fadeImage.color = Color.Lerp(new Color(0.0f, 0.0f, 0.0f, 0.0f), new Color (0.0f, 0.0f, 0.0f, 1.0f), fadeTimeCurrent / fadeTimeMax);
+			}
+		} 
+		else
+		{
+			if (fadeTimeCurrent > 0.0f)
+			{
+				fadeTimeCurrent = Mathf.Clamp(fadeTimeCurrent - Time.deltaTime, 0.0f, fadeTimeMax);
+				fadeImage.color = Color.Lerp (new Color(0.0f, 0.0f, 0.0f, 0.0f), new Color (0.0f, 0.0f, 0.0f, 1.0f), fadeTimeCurrent / fadeTimeMax);
+			}
+		}
+	}
+
+	void StartFade()
+	{
+		FadeOn = true;
+	}
+
+	void StopFade()
+	{
+		FadeOn = false;
+	}
+}
